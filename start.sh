e0b621cb17a37769e55b21eaf56018534270b60d
paperbenni <paperbenni@gmail.com>
2019-04-23 23:06:38 +0200

    travis

--- start.sh
+++ start.sh
@@ -10,18 +10,19 @@ if [ -n "$HMAIL1" ] && [ -n "$HMAIL2" ] &&
     [ -n "$HAPP1" ] && [ -n "$HAPP2" ]; then
     echo "variables found"
 fi
-./login.sh "$HMAIL1" "$HPASS1"
+
+hlogin "$HMAIL1" "$HPASS1"
 
 HTIME1=$(htime "$HAPP1")
 if [ "$HTIME1" = "100" ]; then
     echo "$HMAIL1 fully used, changing to $HMAIL2"
     sleep 5
     heroku ps:scale web=0 -a "$HAPP1"
-    ./login.sh "$HMAIL2" "$HPASS2"
+    hlogin "$HMAIL2" "$HPASS2"
     heroku ps:scale web=1 -a "$HAPP2"
 else
     echo "account 1 still going"
     heroku ps:scale web=1 -a "$HAPP1"
-    ./login.sh "$HMAIL2" "$HPASS2"
+    hlogin "$HMAIL2" "$HPASS2"
     heroku ps:scale web=0 -a "$HAPP2"
 fi
